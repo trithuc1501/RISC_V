1feeec87360c3d76fd594f290bb2b49afc252f78
trithuc1501 <trithuc1152005@gmail.com>
2026-05-31 22:21:35 +0700

    Remove register file reset logic

--- Register/Register.sv
+++ Register/Register.sv
@@ -1,6 +1,5 @@
 module Register (
     input logic clk,
-    input logic rst_n,
     input logic [4:0] Read_register_1,
     input logic [4:0] Read_register_2,
     input logic [4:0] Write_register,
@@ -15,12 +14,8 @@ module Register (
     assign Read_data_1 = (Read_register_1 == '0) ? '0 : Register[Read_register_1];
     assign Read_data_2 = (Read_register_2 == '0) ? '0 : Register[Read_register_2];
 
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            for (int i = 0; i < 32; i++) begin
-                Register[i] <= '0;
-            end
-        end elseif (RegWrite || (Write_register != '0)) begin
+    always @(posedge clk) begin
+        if (RegWrite || (Write_register != '0)) begin
             Register[Write_register] <= Write_data;
         end
     end
